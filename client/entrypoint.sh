cc157311e2f8d5f5c75adfd9b7cb21588792407f
niu atsushi <atsushi.new414@gmail.com>
2023-01-12 17:11:47 +0000

    環境構築ファイル一時修正

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -2,7 +2,6 @@
 
 if [ ! -e './app' ]; then
   echo "set up application..."
-  npm install -g create-next-app@13.0.7
   npx create-next-app app --ts --eslint --use-npm
 else
   echo "already set up application"
